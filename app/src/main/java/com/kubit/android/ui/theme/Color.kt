3370e4b4b0bfb6f59e53ac71b0d5e4b02edfa783
ByeongSu Kim <kimbsu3000@gmail.com>
2023-04-28 16:31:05 +0900

    res: Color Resource 추가

--- app/src/main/java/com/kubit/android/ui/theme/Color.kt
+++ app/src/main/java/com/kubit/android/ui/theme/Color.kt
@@ -6,3 +6,40 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val PrimaryLight = Color(0xFF1C4528)
+val PrimaryDark = Color(0xFF0D0D03)
+
+val SecondaryLight = Color(0xFF2E653D)
+val SecondaryDark = Color(0xFF2E653D)
+
+val TextLight = Color(0xFF202020)
+val TextDark = Color(0xFFD9D9D9)
+val TextGray = Color(0xFF9E9E9E)
+
+val CandleLight = Color(0xFFEEEEEE)
+val CandleDark = Color(0xFF222222)
+
+val BorderLight = Color(0xFFEAEAEA)
+val BorderDark = Color(0xFF222222)
+
+val CoinBlueLight = Color(0xFF2F5FBD)
+val CoinBlueDark = Color(0xFF2F5FBD)
+val CoinRedLight = Color(0xFFB95338)
+val CoinRedDark = Color(0xFFB95338)
+
+val Portfolio001 = Color(0xFF94B24A)
+val Portfolio002 = Color(0xFF1081AC)
+val Portfolio003 = Color(0xFF6B5994)
+val Portfolio004 = Color(0xFFB569A4)
+val Portfolio005 = Color(0xFFF77929)
+val Portfolio006 = Color(0xFFFFB23A)
+val Portfolio007 = Color(0xFFC6926B)
+val Portfolio008 = Color(0xFFD5B252)
+val Portfolio009 = Color(0xFF638EAD)
+val Portfolio010 = Color(0xFFBDBEC5)
+
+val TransactionBlueLight = Color(0xFFECF3FB)
+val TransactionBlueDark = Color(0xFF081D3A)
+val TransactionRedLight = Color(0xFFFBF1F0)
+val TransactionRedDark = Color(0xFF241A23)
