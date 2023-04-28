1a55875e5d1c9b81df0e2185cc1af8a41e511cb0
ByeongSu Kim <kimbsu3000@gmail.com>
2023-04-28 16:42:27 +0900

    res: Color Resource 변경

--- app/src/main/java/com/kubit/android/ui/theme/Color.kt
+++ app/src/main/java/com/kubit/android/ui/theme/Color.kt
@@ -2,11 +2,6 @@ package com.kubit.android.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
-
 val PrimaryLight = Color(0xFF1C4528)
 val PrimaryDark = Color(0xFF0D0D03)
 
